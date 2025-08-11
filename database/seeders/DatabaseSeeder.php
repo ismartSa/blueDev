c9495ee4a045f46bf084f4675b823849bd4db989
BlueSA <blueSA@HSAs-MacBook-Pro.local>
2025-08-12 00:34:58 +0300

    chore(database): add CategoriesTableSeeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             RoleSeeder::class,
             UserSeeder::class,
+            CategoriesTableSeeder::class,
             CourseSeeder::class,
             QuizSeeder::class,
             LectureSeeder::class,
